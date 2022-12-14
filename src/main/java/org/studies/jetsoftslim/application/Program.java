486ae4eb8034de118bf17026c3e631d899e790c8
koxx4 <piotr815@poczta.fm>
2022-12-14 22:21:45 +0100

    working

--- src/main/java/org/studies/jetsoftslim/application/Program.java
+++ src/main/java/org/studies/jetsoftslim/application/Program.java
@@ -1,21 +1,59 @@
 package org.studies.jetsoftslim.application;
 
+import org.studies.jetsoftslim.application.infrastructure.*;
 import org.studies.jetsoftslim.controller.FlightController;
+import org.studies.jetsoftslim.controller.FlightFormValidator;
 import org.studies.jetsoftslim.controller.ReservationController;
 import org.studies.jetsoftslim.model.*;
 
 public class Program {
 
-    private Repository<Flight> flightRepository;
-    private Repository<Pilot> pilotRepository;
-    private Repository<Vehicle> vehicleRepository;
-    private Repository<Reservation> reservationRepository;
-    private FlightController flightController;
-    private ReservationController reservationController;
+    private final Repository<Flight> flightRepository;
+    private final Repository<Pilot> pilotRepository;
+    private final Repository<Vehicle> vehicleRepository;
+    private final Repository<Reservation> reservationRepository;
+    private final FlightController flightController;
+    private final ReservationController reservationController;
+    private final EntityIdGenerator entityIdGenerator;
+    private final FlightNumberGenerator flightNumberGenerator;
+    private final ReservationNumberGenerator reservationNumberGenerator;
+    private final FlightFormValidator flightFormValidator;
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        new Program();
+    }
+
+    private Program() {
+
+        entityIdGenerator = new GlobalEntityIdGenerator();
+        flightNumberGenerator = new CityBasedFlightNumberGenerator();
+        flightFormValidator = new FlightFormValidator();
+        reservationNumberGenerator = new ReservationNumberGenerator();
+
+        flightRepository = new InMemoryFlightRepository(entityIdGenerator);
+        pilotRepository = new InMemoryPilotRepository(entityIdGenerator);
+        vehicleRepository = new InMemoryVehicleRepository(entityIdGenerator);
+        reservationRepository = new InMemoryReservationRepository(entityIdGenerator);
+
+        flightController = new FlightController(
+                flightRepository,
+                pilotRepository,
+                vehicleRepository,
+                flightNumberGenerator,
+                flightFormValidator
+                );
+
+        reservationController = new ReservationController(
+                reservationRepository,
+                flightRepository,
+                reservationNumberGenerator);
+
+        generatePilots();
+        generateVehicles();
+        generateFlights();
+        generateReservations();
+        showFlightsAndReservations();
     }
 
     public void generatePilots() {
